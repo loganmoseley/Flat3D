863dea8a9df646277b51bed8eb4c7559a5ec00ab
Logan Moseley <git@loganmoseley.com>
2016-12-11 17:03:33 -0500

    Initial commit, macOS SpriteKit project

---
+++ Flat3D/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Flat3D
+//
+//  Created by Logan Moseley on 12/11/16.
+//  Copyright © 2016 Logan Moseley. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Flat3D/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Flat3D
+//
+//  Created by Logan Moseley on 12/11/16.
+//  Copyright © 2016 Logan Moseley. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
